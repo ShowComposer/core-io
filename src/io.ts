857a8c43ca106abea0b9633a7aec0b20a69516af
Marvin Gaube <dev@marvingaube.de>
2019-05-01 17:47:01 +0200

    Create DMXNet Sender objects

--- src/io.ts
+++ src/io.ts
@@ -1,3 +1,22 @@
 import { Datalib } from "@showcomposer/datalib";
+import dmxlib = require("dmxnet");
 
 const data = new Datalib();
+const dmxnet = new dmxlib.dmxnet({sName: "ShowComposer", lName: "ShowComposer IO module"});
+const dmxnetSenders = {};
+
+data.dump('io.artnet');
+const artnetEvent = data.subscribe('io.artnet');
+artnetEvent.on('data',()=>{
+  handleArtNet();
+});
+function handleArtNet() {
+  const anData=data.data["io"]["artnet"];
+  if(anData) {
+    Object.keys(anData).forEach((s) => {
+      if(!dmxnetSenders[s]) {
+        dmxnetSenders[s]=dmxnet.newSender({});
+      }
+    });
+  }
+}
---
+++ src/test.ts
@@ -0,0 +1,5 @@
+import { Datalib } from "@showcomposer/datalib";
+
+const data = new Datalib();
+
+data.set("io.artnet.channel.0", 1);
