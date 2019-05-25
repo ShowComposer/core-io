025933fc5f352cac913f085220545df531f6e5ed
Marvin Gaube <dev@marvingaube.de>
2019-05-25 16:11:40 +0200

    Add IP/Port/Destination/Subnet to options

--- src/io.ts
+++ src/io.ts
@@ -9,14 +9,14 @@ import dmxlib = require("dmxnet");
 // SC Data
 const data = new Datalib();
 // ArtNet
-const dmxnet = new dmxlib.dmxnet({sName: "ShowComposer", lName: "ShowComposer IO module"});
+const dmxnet = new dmxlib.dmxnet({ sName: "ShowComposer", lName: "ShowComposer IO module" });
 const dmxnetSenders = {};
 
 // Get all existing artnet data
 data.dump("io.artnet");
 const artnetEvent = data.subscribe("io.artnet");
 // Subscribe to ArtNet changes
-artnetEvent.on("data", (k) => {
+artnetEvent.on("data", (k, d) => {
   handleArtNet(k);
 });
 // Handle incoming artnet data
@@ -29,14 +29,24 @@ function handleArtNet(k) {
     // Create sender instance if not existing
     Object.keys(anData).forEach((s) => {
       if (!dmxnetSenders[s]) {
-        dmxnetSenders[s] = dmxnet.newSender({});
-        Logging.log("New DMXNET sender");
+        // Set DMXNET-Sender parameters
+        const dmxnetIP = anData[s].ip || "255.255.255.255";
+        const dmxnetSubnet = anData[s].subnet || 0;
+        const dmxnetUniverse = anData[s].universe || 0;
+        const dmxnetNet = anData[s].net || 0;
+        const dmxnetPort = anData[s].port || 6454;
+        dmxnetSenders[s] = dmxnet.newSender({ip: dmxnetIP, net: dmxnetNet,
+          port: dmxnetPort, subnet: dmxnetSubnet, universe: dmxnetUniverse,
+           });
+        Logging.log("New DMXNET sender to " + dmxnetIP + ":" +
+        dmxnetPort + " N:" + dmxnetNet + " S:" + dmxnetSubnet + " U:" +
+        dmxnetUniverse);
       }
       Logging.debug(anData);
       // Send channels
       if (anData[s].channel) {
         Object.keys(anData[s].channel).forEach((c) => {
-        dmxnetSenders[s].prepChannel(c, parseInt(anData[s].channel[c], 10));
+          dmxnetSenders[s].prepChannel(c, parseInt(anData[s].channel[c], 10));
         });
         dmxnetSenders[s].transmit();
       }
--- src/test.ts
+++ src/test.ts
@@ -1,9 +1,15 @@
 import { Datalib } from "@showcomposer/datalib";
 
 const data = new Datalib();
-
+data.set("io.artnet.0.ip", "10.11.0.11");
+data.set("io.artnet.0.universe", 1);
 data.set("io.artnet.0.channel.1", 255);
 data.set("io.artnet.0.channel.255", 255);
+for (let i = 0; i < 512; i += 3) {
+  data.set("io.artnet.0.channel." + i, 0);
+  data.set("io.artnet.0.channel." + (i + 1), 0);
+  data.set("io.artnet.0.channel." + (i + 2), 255);
+}
 setTimeout(() => {
   data.set("io.artnet.0.channel.511", 255);
 }, 1000);
